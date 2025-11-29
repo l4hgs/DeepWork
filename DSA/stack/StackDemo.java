7eebb72dd222ebdbc60a70514d749df2fb0ec7f9
rcccantos <riccocantos@gmail.com>
2025-11-29 08:02:04 +0800

    javastacks

---
+++ DSA/stack/StackDemo.java
@@ -0,0 +1,22 @@
+import java.util.Iterator;
+import java.util.Stack;
+
+class StackDemo {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<Integer>();
+        st.push(98);
+        st.push(99);
+        st.push(100);
+        // pop at the top
+        st.pop();
+
+        System.out.println("Stack ELements");
+        
+        // di nababago
+        for (Iterator<Integer> iter = st.iterator(); iter.hasNext();) {
+            System.out.println(iter.next());
+        }
+        int top = st.peek();
+        System.out.println("Top of stack: " + top);
+    }
+}
